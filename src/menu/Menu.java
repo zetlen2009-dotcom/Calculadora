856a02aed2d79ebe69e28910b380acb579532fd6
Compu17 <Compu17@CPU-17>
2026-03-24 15:40:19 -0600

    prueba2

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) {
     Scanner teclado = new Scanner(System.in);
+    //suma clasesuma = new suma();
+    //resta claseresta = new resta();
         
         System.out.println("Ingrese el primer numero: ");
             int a = teclado.nextInt();
         System.out.println("Ingrese el segundo numero: ");
             int b = teclado.nextInt();
         
-    //suma clasesuma = new suma();
-    //resta claseresta = new resta();
-    
     System.out.println("---------SUMA------------");
      //   System.out.println(clasesuma);
      System.out.println("---------RESTA-----------");
